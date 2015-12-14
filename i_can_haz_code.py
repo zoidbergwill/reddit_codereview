6f34fe4d00f30a771dd8311759e845222f434148
William Stewart <zoidbergwill@gmail.com>
2015-12-14 19:39:52 +0200

    Refactor

--- i_can_haz_code.py
+++ i_can_haz_code.py
@@ -1,51 +1,74 @@
 #!/usr/bin/env python
+import random
+from math import sqrt
 
 import matplotlib.pyplot as plt
-import math
-import random
 
-ds = [random.randint(10,100) for i in range(0,100)]
+data = [random.randint(10, 100) for i in range(0, 100)]
 
-def anal(ds=[]):
-    if not isinstance(ds,list):
-        return('no list... why no list???')
-    if not isinstance(random.choice(ds), int):
-        return('At least one element of ds is not an int.')
-    def mean(ds):
-        return(sum(ds)/len(ds))
-    def median(ds):
-        ds = sorted(ds)
-        if len(ds) < 1:
-            return (None)
-        if len(ds) %2 == 1:
-            return (ds[((len(ds)+1)/2)-1])
+
+class Analysis(object):
+    def __init__(self, data):
+        data = data or []
+        if not isinstance(data, list):
+            return 'no list... why no list???'
+        if any(not isinstance(d, int) for d in data):
+            return 'At least one element of ds is not an int.'
+        self.data = data
+
+    def mean(self):
+        return sum(self.data) / len(self.data)
+
+    def median(self):
+        data = sorted(self.data)
+        ds_length = len(data)
+        if ds_length < 1:
+            return None
+        elif ds_length % 2 == 1:
+            return self.data[((ds_length + 1) / 2) - 1]
         else:
-            return float(sum(ds[(len(ds)/2)-1:(len(ds)/2)+1]))/2.0
-    def mode(ds):
-        return(max(set(ds), key=ds.count))
-    def std(ds):
-        return(math.sqrt(sum([(val - mean(ds))**2 for val in ds])/(len(ds) - 1)))
-    def get_max(ds):
-        return(max(ds))
-    def get_min(ds):
-        return(min(ds))
-    def get_range(ds):
-        return(max(ds)-min(ds))
-    def graphit(ds,mean=mean(ds),median=median(ds),mode=mode(ds),maxs=get_max(ds),get_min=get_min(ds),get_range=get_range(ds),std=std(ds)):
-        means = [mean for i in ds]
-        medians = [median for i in ds]
-        modes = [mode for i in ds]
-        maxes = [maxs for i in ds]
-        mins = [get_min for i in ds]
-        stdsp = [mean + std for i in ds]
-        stdsm = [mean - std for i in ds]
-        ranges = [get_range for i in ds]
-        [plt.plot(i) for i in [ds,means,medians,modes,maxes,mins,ranges,stdsp,stdsm]]
-    graphit(ds)
-    plt.title('mean = %s median = %s mode = %s std = %s max = %s min = %s range = %s'%(mean(ds),median(ds),mode(ds),std(ds),get_max(ds),get_min(ds),get_range(ds)))
-#   plt.savefig('./tmp.png')
-    plt.show()
+            return float(sum(data[(ds_length / 2) - 1:(ds_length / 2) + 1])) / 2.0
+
+    def mode(self):
+        return max(set(self.data), key=self.data.count)
+
+    def std_dev(self):
+        return sqrt(sum((val - self.mean()) ** 2 for val in self.data) / (len(self.data) - 1))
+
+    def get_max(self):
+        return max(self.data)
+
+    def get_min(self):
+        return min(self.data)
+
+    def get_range(self):
+        return max(self.data) - min(self.data)
+
+    def graph_it(self, data=None, mean=None, median=None, mode=None, maximum=None, minimum=None, data_range=None, std_dev=None):
+        self.data = data or self.data
+        mean = mean or self.mean()
+        median = median or self.median()
+        mode = mode or self.mode()
+        maximum = maximum or self.get_max()
+        minimum = minimum or self.get_min()
+        data_range = data_range or self.get_range()
+        std_dev = std_dev or self.std_dev()
+
+        means = [mean for i in self.data]
+        medians = [median for i in self.data]
+        modes = [mode for i in self.data]
+        maxes = [maximum for i in self.data]
+        mins = [minimum for i in self.data]
+        stdsp = [mean + std_dev for i in self.data]
+        stdsm = [mean - std_dev for i in self.data]
+        ranges = [data_range for i in self.data]
+        for i in (self.data, means, medians, modes, maxes, mins, ranges, stdsp, stdsm):
+            plt.plot(i)
+        plt.title(
+            'mean = {} median = {} mode = {} std = {} max = {} min = {} range = {}'.format(
+                mean, median, mode, std_dev, maximum, minimum, data_range))
+        #  plt.savefig('./tmp.png')
+        plt.show()
 
 
-
-anal(ds)
+Analysis(data).graph_it()
